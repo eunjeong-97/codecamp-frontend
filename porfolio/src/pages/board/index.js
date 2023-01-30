3cabe2994eeeeb2c63bdeba6929803f6b5649131
eunjeong-97 <eoss7777@naver.com>
2023-01-30 14:38:40 +0900

    230130: 게시판 메인화면 마무리

--- porfolio/src/pages/board/index.js
+++ porfolio/src/pages/board/index.js
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import { gql, useQuery } from "@apollo/client";
-import styled from "@emotion/styled";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 import Header from "@/components/common/Header";
 import Banner from "@/components/common/Banner";
 import Card from "@/components/board/Card";
 import Loading from "@/components/common/Loading";
-import { colors, fonts, Shadow } from "@/styles/globalStyle";
+import * as S from "@/styles/board/index";
 
 const FETCH_BEST_BOARDS = gql`
   query {
@@ -43,25 +43,58 @@ const FETCH_BOARDS = gql`
   }
 `;
 
+const FETCH_BOARDS_COUNT = gql`
+  query fetchBoardsCount(
+    $endDate: DateTime
+    $startDate: DateTime
+    $search: String
+  ) {
+    fetchBoardsCount(endDate: $endDate, startDate: $startDate, search: $search)
+  }
+`;
+
 const BoardItem = ({ num, title, writer, date, border }) => (
-  <Row border={border}>
-    <Column flex={7.5}>{num}</Column>
-    <Column flex={72.5}>{title}</Column>
-    <Column flex={10}>{writer}</Column>
-    <Column flex={10}>{date}</Column>
-  </Row>
+  <S.Row border={border}>
+    <S.Column flex={7} bold={typeof num === "string"}>
+      {num}
+    </S.Column>
+    <S.Column flex={70} bold={typeof num === "string"}>
+      {title}
+    </S.Column>
+    <S.Column flex={15} bold={typeof num === "string"}>
+      {writer}
+    </S.Column>
+    <S.Column flex={15} bold={typeof num === "string"}>
+      {date}
+    </S.Column>
+  </S.Row>
+);
+
+const PaginationItem = ({ num, setPage, selected, last }) => (
+  <S.Num onClick={() => setPage(num)} last={last} selected={selected}>
+    {num}
+  </S.Num>
 );
 
 export default () => {
-  const [input, setInput] = useState("");
+  const router = useRouter();
+  const [search, setSearch] = useState("");
   const [dateInput, setDateInput] = useState("");
+  const [page, setPage] = useState(1);
   const { data: bestData } = useQuery(FETCH_BEST_BOARDS);
   const { data } = useQuery(FETCH_BOARDS, {
-    variables: { endDate: null, startDate: null, search: "", page: 1 },
+    variables: { endDate: null, startDate: null, search, page },
+  });
+  const { data: boardCountData } = useQuery(FETCH_BOARDS_COUNT);
+  const totalCount = Math.ceil(boardCountData?.fetchBoardsCount / 10);
+  const pages = new Array(9).fill(undefined).map((_, index) => {
+    let plusNum = 2;
+    if (page > 10) plusNum = plusNum + (page - 10);
+    return index + plusNum;
   });
 
   const onSearch = async () => {
-    if (!input) {
+    if (!search) {
       alert("검색어를 입력해주세요.");
       return;
     }
@@ -69,19 +102,18 @@ export default () => {
       alert("날짜를 입력해주세요.");
       return;
     }
-    alert({ input, dateInput });
+    alert({ search, dateInput });
   };
 
-  console.log(data?.fetchBoards);
   return (
     <div style={{ paddingBottom: 300 }}>
       <Header />
       <Banner />
 
       {/* 베스트 */}
-      <Title>베스트 게시글</Title>
+      <S.Title>베스트 게시글</S.Title>
       {bestData?.fetchBoardsOfTheBest ? (
-        <Row>
+        <S.Row>
           {bestData.fetchBoardsOfTheBest.map((item, idx) => (
             <Card
               key={item._id}
@@ -93,129 +125,109 @@ export default () => {
               likeNum={item.likeCount}
             />
           ))}
-        </Row>
+        </S.Row>
       ) : (
         <Loading />
       )}
 
       {/* 검색창 */}
-      <Row mt={80}>
-        <InputWrap>
+      <S.Row mt={80}>
+        <S.InputWrap>
           <Image
             src={require("../../../public/image/ico_search.png")}
             width={24}
             height={24}
           />
-          <Input
+          <S.Input
             type="text"
-            onChange={({ target }) => setInput(target.value)}
+            onChange={({ target }) => setSearch(target.value)}
           />
-        </InputWrap>
-        <DateInput
+        </S.InputWrap>
+        <S.DateInput
           type="text"
           onChange={({ target }) => setDateInput(target.value)}
           placeholder="YYYY.MM.DD ~ YYYY.MM.DD"
         />
-        <SearchBtn onClick={onSearch}>검색하기</SearchBtn>
-      </Row>
+        <S.SearchBtn onClick={onSearch}>검색하기</S.SearchBtn>
+      </S.Row>
 
       {/* 게시물 */}
-      <Boards>
+      <S.Boards>
         <BoardItem num="번호" title="제목" writer="작성자" date="날짜" border />
+        {data?.fetchBoards &&
+          [...data?.fetchBoards].reverse().map((boardItem, idx) => {
+            const { title, writer, createdAt, _id: id } = boardItem;
+            const year = createdAt.slice(0, 4);
+            const month = createdAt.slice(5, 7);
+            const date = createdAt.slice(8, 10);
+            return (
+              <BoardItem
+                key={id}
+                num={10 - idx}
+                title={title}
+                writer={writer}
+                date={`${year}.${month}.${date}`}
+                border={data?.fetchBoards.length !== idx + 1}
+              />
+            );
+          })}
+      </S.Boards>
+      <S.Footer>
+        {!isNaN(totalCount) && (
+          <S.Pagination>
+            <S.ImageBtn
+              mr={20}
+              disabled={page === 1}
+              onClick={() => setPage((_page) => _page - 1)}
+            >
+              <Image
+                src={require("../../../public/image/ico_arrow_left.png")}
+                alt="왼쪽화살표"
+                width={24}
+                height={24}
+              />
+            </S.ImageBtn>
+            <PaginationItem num={1} setPage={setPage} selected={page === 1} />
+            {page > 10 && <div style={{ marginRight: 20 }}>...</div>}
+            {pages.map((num) => (
+              <PaginationItem
+                key={num}
+                num={num}
+                setPage={setPage}
+                selected={page === num}
+              />
+            ))}
+            <div style={{ marginRight: 20 }}>...</div>
+            <PaginationItem
+              num={totalCount}
+              setPage={setPage}
+              selected={page === totalCount}
+              last
+            />
+            <S.ImageBtn
+              ml={20}
+              disabled={page === totalCount}
+              onClick={() => setPage((_page) => _page + 1)}
+            >
+              <Image
+                src={require("../../../public/image/ico_arrow_right.png")}
+                alt="오른쪽화살표"
+                width={24}
+                height={24}
+              />
+            </S.ImageBtn>
+          </S.Pagination>
+        )}
 
-        <BoardItem
-          num="1"
-          title="게시물 제목입니다."
-          writer="박은정"
-          date="2020.09.28"
-          border
-        />
-      </Boards>
+        <S.CreateBtn onClick={() => router.push("/board/create")}>
+          <Image
+            src={require("../../../public/image/ico_modify.png")}
+            width={24}
+            height={24}
+          />
+          <span>게시물 등록하기</span>
+        </S.CreateBtn>
+      </S.Footer>
     </div>
   );
 };
-
-const Title = styled.p`
-  text-align: center;
-  font-family: ${fonts.bold};
-  color: ${colors.black};
-  font-size: 36px;
-  margin-bottom: 40px;
-`;
-const Row = styled.div`
-  display: flex;
-  justify-content: space-between;
-  max-width: 1200px;
-  align-items: center;
-  margin: 0 auto;
-  flex-wrap: wrap;
-  margin-top: ${({ mt }) => mt}px;
-  border-bottom: ${({ border }) => `1px solid ${border && colors.gray04}`};
-  @media screen and (max-width: 1199px) {
-    margin: 0 20px;
-  }
-`;
-
-const InputWrap = styled.div`
-  display: flex;
-  align-items: center;
-  flex: 1;
-  height: 52px;
-  padding-left: 18px;
-  background-color: ${colors.gray06};
-  border-radius: 10px;
-`;
-
-const Input = styled.input`
-  display: flex;
-  flex: 1;
-  height: 52px;
-  padding: 0 18px 0 10px;
-  background-color: ${colors.gray06};
-  border: none;
-  border-radius: 10px;
-  outline: none;
-  font-family: ${fonts.medium};
-  font-size: 16px;
-  color: ${colors.black};
-`;
-
-const DateInput = styled.input`
-  height: 52px;
-  margin: 0 44px;
-  padding: 0 16px;
-  background-color: ${colors.white};
-  border: 1px solid ${colors.gray04};
-  outline: none;
-  font-family: ${fonts.regular};
-  color: ${colors.gray04};
-  font-size: 16px;
-  text-align: center;
-`;
-const SearchBtn = styled.button`
-  height: 52px;
-  padding: 0 16px;
-  background-color: ${colors.black};
-  border: none;
-  border-radius: 10px;
-  font-family: ${fonts.medium};
-  font-size: 16px;
-  color: ${colors.white};
-`;
-
-const Boards = styled.div`
-  border-top: 1px solid ${colors.black};
-  border-bottom: 1px solid ${colors.black};
-  margin: 40px auto 60px auto;
-  max-width: 1200px;
-  @media screen and (max-width: 1199px) {
-    margin: 40px 20px 60px 20px;
-  }
-`;
-const Column = styled.div`
-  padding: 10px 0;
-  text-align: center;
-  display: flex;
-  flex: ${({ flex }) => flex};
-  justify-content: center;
-`;
---
+++ porfolio/src/styles/board/index.js
@@ -0,0 +1,150 @@
+import styled from "@emotion/styled";
+import { colors, fonts, Shadow } from "@/styles/globalStyle";
+import Image from "next/image";
+
+export const Title = styled.p`
+  text-align: center;
+  font-family: ${fonts.bold};
+  color: ${colors.black};
+  font-size: 36px;
+  margin-bottom: 40px;
+`;
+export const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+  max-width: 1200px;
+  align-items: center;
+  margin: 0 auto;
+  flex-wrap: wrap;
+  margin-top: ${({ mt }) => mt}px;
+  border-bottom: ${({ border }) => `1px solid ${border && colors.gray04}`};
+  @media screen and (max-width: 1199px) {
+    padding: 0 20px;
+  }
+`;
+
+export const InputWrap = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  height: 52px;
+  padding-left: 18px;
+  background-color: ${colors.gray06};
+  border-radius: 10px;
+`;
+
+export const Input = styled.input`
+  display: flex;
+  flex: 1;
+  height: 52px;
+  padding: 0 18px 0 10px;
+  background-color: ${colors.gray06};
+  border: none;
+  border-radius: 10px;
+  outline: none;
+  font-family: ${fonts.medium};
+  font-size: 16px;
+  color: ${colors.black};
+`;
+
+export const DateInput = styled.input`
+  height: 52px;
+  margin: 0 44px;
+  padding: 0 16px;
+  background-color: ${colors.white};
+  border: 1px solid ${colors.gray04};
+  outline: none;
+  font-family: ${fonts.regular};
+  color: ${colors.gray04};
+  font-size: 16px;
+  text-align: center;
+`;
+export const SearchBtn = styled.button`
+  height: 52px;
+  padding: 0 16px;
+  background-color: ${colors.black};
+  border: none;
+  border-radius: 10px;
+  font-family: ${fonts.medium};
+  font-size: 16px;
+  color: ${colors.white};
+`;
+
+export const Boards = styled.div`
+  border-top: 1px solid ${colors.black};
+  border-bottom: 1px solid ${colors.black};
+  margin: 40px auto 40px auto;
+  max-width: 1200px;
+  @media screen and (max-width: 1199px) {
+    margin: 40px 20px 40px 20px;
+  }
+`;
+export const Column = styled.div`
+  padding: 20px 0;
+  text-align: center;
+  display: flex;
+  flex: ${({ flex }) => flex};
+  justify-content: center;
+  font-family: ${({ bold }) => (bold ? fonts.bold : fonts.regular)};
+`;
+
+export const Footer = styled.div`
+  max-width: 1200px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 50px;
+  margin: 0 auto;
+  @media screen and (max-width: 1199px) {
+    margin: 0 20px;
+  }
+`;
+
+export const Pagination = styled.div`
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 24px;
+`;
+
+export const CreateBtn = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 14px 16px;
+  background-color: ${colors.white};
+  border: 1px solid ${colors.gray06};
+  border-radius: 10px;
+
+  span {
+    font-family: ${fonts.medium};
+    font-size: 16px;
+    text-align: center;
+  }
+`;
+
+export const Num = styled.button`
+  border: none;
+  background-color: transparent;
+  font-family: ${colors.regular};
+  color: ${({ selected }) => (selected ? colors.yellow : colors.gray02)};
+  text-decoration: underline;
+  font-size: ${({ selected }) => (selected ? 18 : 16)}px;
+  padding: 0;
+  cursor: pointer;
+  margin-right: ${({ last }) => !last && 20}px;
+  height: 100%;
+`;
+
+export const ImageBtn = styled.button`
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  padding: 0;
+  height: 100%;
+  margin-left: ${({ ml }) => ml || 0}px;
+  margin-right: ${({ mr }) => mr || 0}px;
+`;
